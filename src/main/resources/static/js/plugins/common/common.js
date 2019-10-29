aff24796ec732f42008021bb9e5ee8bf8a9ed754
selfvolinghis <2629106344@qq.com>
2019-10-29 10:56:26 +0800

    解决请求缓存问题

--- src/main/resources/static/js/plugins/common/common.js
+++ src/main/resources/static/js/plugins/common/common.js
@@ -1,3 +1,5 @@
+
+
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
@@ -49,6 +51,19 @@ function randomString() {
 	 　　}
 	　　return pwd;
 }
+//添加请求拦截器
+axios.interceptors.request.use(function (config) {
+	if(config.url.indexOf("?")>0){
+		config.url=config.url+"&tt="+Math.random();
+	}else{
+		config.url=config.url+"?tt="+Math.random();
+	}
+    // 在发送请求之前做些什么
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
 var GPageModel={
 
 		info:function(config){
